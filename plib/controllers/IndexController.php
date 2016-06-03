fd7d9e772849cf76faceaf6f79cc032ad022c44f
Aleksei Vokhmianin <vaazanuda@gmail.com>
2016-06-03 13:17:35 +0600

    fixes

--- plib/controllers/IndexController.php
+++ plib/controllers/IndexController.php
@@ -11,27 +11,23 @@ class IndexController extends pm_Controller_Action
 
     public function enabledebugAction()
     {
-//        if ($this->_request->isPost()) {
             $result = pm_ApiCli::callSbin("enable-debug.cmd");
             if ($result['code'] !== 0) {
                 $this->_status->addError("Failed to enable debug logging: {$result['stdout']} {$result['stderr']}");
             } else {
                 $this->_status->addMessage('info', 'Debug logging enabled.');
             }
-//        }
         $this->_redirect('index/index');
     }
 
     public function disabledebugAction()
     {
-//        if ($this->_request->isPost()) {
             $result = pm_ApiCli::callSbin("disable-debug.cmd");
             if ($result['code'] !== 0) {
                 $this->_status->addError("Failed to enable debug logging: {$result['stdout']} {$result['stderr']}");
             } else {
                 $this->_status->addMessage('info', 'Debug logging disabled.');
             }
-//        }
         $this->_redirect('index/index');
     }
 }

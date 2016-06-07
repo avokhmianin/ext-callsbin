e4967d2432dfb0f736b0aa01eaf6fa4f56a41e75
Aleksei Vokhmianin <vaazanuda@gmail.com>
2016-06-07 09:52:51 +0600

    Update IndexController.php

--- plib/controllers/IndexController.php
+++ plib/controllers/IndexController.php
@@ -2,11 +2,8 @@
 // Copyright 1999-2016. Parallels IP Holdings GmbH.
 class IndexController extends pm_Controller_Action
 {
-    protected $_accessLevel = 'admin';
-
     public function indexAction()
     {
-
     }
 
     public function enabledebugAction()
